20bdc9c9050b3d378ba22c9af07e49bfc6fbc0f5
Andreas Linnert <andreas@linnertmedia.de>
2024-03-29 23:24:46 +0100

    Create helper functions for attributes and targets

--- public/app/js/components/numeric-setting.js
+++ public/app/js/components/numeric-setting.js
@@ -1,44 +1,81 @@
+import { createAttr, floatAttr, intAttr, stringAttr } from '../lib/components/attributes.js'
+import { createTargets } from '../lib/components/targets.js'
+
 export class NumericSetting extends HTMLElement {
-  get label() {
-    return this.getAttribute('label')
-  }
-  set label(value) {
-    this.setAttribute('label', value)
-  }
+  /** @type {import('../lib/components/attributes.js').DynamicAttr<string>} */
+  #label = createAttr(this, 'label', { type: stringAttr })
 
-  get value() {
-    return this.getAttribute('value')
-  }
-  set value(value) {
-    this.setAttribute('value', value)
-  }
+  /** @type {import('../lib/components/attributes.js').DynamicAttr<number>} */
+  #decimalPoints = createAttr(this, 'decimal-points', {
+    type: intAttr,
+    defaultValue: 0,
+  })
 
-  get min() {
-    return this.getAttribute('min')
-  }
-  set min(value) {
-    this.setAttribute('min', value)
-  }
+  /** @type {import('../lib/components/attributes.js').DynamicAttr<number>} */
+  #min = createAttr(this, 'min', {
+    type: floatAttr(this.#decimalPoints.get()),
+    defaultValue: -Infinity,
+  })
 
-  get max() {
-    return this.getAttribute('max')
-  }
-  set max(value) {
-    this.setAttribute('max', value)
-  }
+  /** @type {import('../lib/components/attributes.js').DynamicAttr<number>} */
+  #max = createAttr(this, 'max', {
+    type: floatAttr(this.#decimalPoints.get()),
+    defaultValue: Infinity,
+  })
+
+  /** @type {import('../lib/components/attributes.js').DynamicAttr<number>} */
+  #step = createAttr(this, 'step', {
+    type: floatAttr(this.#decimalPoints.get()),
+    defaultValue: 1,
+  })
+
+  /** @type {import('../lib/components/attributes.js').DynamicAttr<number>} */
+  #value = createAttr(this, 'value', {
+    type: floatAttr(this.#decimalPoints.get()),
+    defaultValue: 0,
+  })
+
+  // value, decrement-value, increment-value
+  #targets = createTargets(this, 'settings-menu-item')
 
   connectedCallback() {
     this.classList.add('settings-menu-item', 'settings-menu-item/type:numeric')
+
+    this.#value.onChange((value) => {
+      this.#targets
+        .getOne('decrement-value')
+        .toggleAttribute('disabled', value === this.#min.getRaw())
+      this.#targets
+        .getOne('increment-value')
+        .toggleAttribute('disabled', value === this.#max.getRaw())
+    })
+
+    this.innerHTML = `
+      <div>${this.#label.get()}</div>
+      <div class="settings-menu-item__value">${this.#value.get()}</div>
+      <button class="settings-menu-item__decrement-value">-</button>
+      <button class="settings-menu-item__increment-value">+</button>
+    `
+
+    this.#targets.getOne('decrement-value').addEventListener('click', () => {
+      this.#value.update((v) => v - this.#step.get())
+      this.update()
+    })
+
+    this.#targets.getOne('increment-value').addEventListener('click', () => {
+      this.#value.update((v) => v + this.#step.get())
+      this.update()
+    })
+
+    this.update()
+  }
+
+  attributeChangedCallback() {
     this.update()
   }
 
   update() {
-    this.innerHTML = `
-      <div>${this.label}</div>
-      <div>${this.value}</div>
-      <div>-</div>
-      <div>+</div>
-    `
+    this.#targets.getOne('value').textContent = this.#value.getRaw()
   }
 }
 
